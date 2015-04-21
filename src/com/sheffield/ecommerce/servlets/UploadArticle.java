473c39fc4f087771cfee1edd8642dbaaa0a5891b
Tudor Sirbu <tdsirbu1@sheffield.ac.uk>
2015-04-21 17:57:52 +0100

    working version of the upload

--- src/com/sheffield/ecommerce/servlets/UploadArticle.java
+++ src/com/sheffield/ecommerce/servlets/UploadArticle.java
@@ -1,31 +1,113 @@
 package com.sheffield.ecommerce.servlets;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.Iterator;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
 
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.apache.commons.io.FilenameUtils;
 import org.hibernate.Session;
 import org.hibernate.exception.ConstraintViolationException;
 
 import com.sheffield.ecommerce.exceptions.InvalidModelException;
 import com.sheffield.ecommerce.models.SessionFactoryUtil;
 import com.sheffield.ecommerce.models.User;
-   
+
 public class UploadArticle extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	private static final Logger LOGGER = Logger.getLogger(UploadArticle.class.getName());
+	private static final Logger LOGGER = Logger.getLogger(UploadArticle.class
+			.getName());
 
-	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		RequestDispatcher requestDispatcher = request.getRequestDispatcher("jsp/upload_article.jsp");
+	// location to store file uploaded
+    private static final String UPLOAD_DIRECTORY = "uploads";
+ 
+    // upload settings
+    private static final int MEMORY_THRESHOLD   = 1024 * 1024 * 3;  // 3MB
+    private static final int MAX_FILE_SIZE      = 1024 * 1024 * 40; // 40MB
+    private static final int MAX_REQUEST_SIZE   = 1024 * 1024 * 50; // 50MB
+	
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		RequestDispatcher requestDispatcher = request
+				.getRequestDispatcher("jsp/upload_article.jsp");
 		requestDispatcher.forward(request, response);
 	}
-	
-	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {	
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		RequestDispatcher requestDispatcher;
 		
-		
+		// checks if the request actually contains upload file
+        if (!ServletFileUpload.isMultipartContent(request)) {
+            return;
+        }
+ 
+        // configures upload settings
+        DiskFileItemFactory factory = new DiskFileItemFactory();
+        // sets memory threshold - beyond which files are stored in disk
+        factory.setSizeThreshold(MEMORY_THRESHOLD);
+        // sets temporary location to store files
+        factory.setRepository(new File(System.getProperty("java.io.tmpdir")));
+ 
+        ServletFileUpload upload = new ServletFileUpload(factory);
+         
+        // sets maximum size of upload file
+        upload.setFileSizeMax(MAX_FILE_SIZE);
+         
+        // sets maximum size of request (include file + form data)
+        upload.setSizeMax(MAX_REQUEST_SIZE);
+ 
+        // constructs the directory path to store upload file
+        // this path is relative to application's directory
+        String uploadPath = getServletContext().getRealPath("")
+                + File.separator + UPLOAD_DIRECTORY;
+         
+        // creates the directory if it does not exist
+        File uploadDir = new File(uploadPath);
+        if (!uploadDir.exists()) {
+            uploadDir.mkdir();
+        }
+ 
+        try {
+            // parses the request's content to extract file data
+            @SuppressWarnings("unchecked")
+            List<FileItem> formItems = upload.parseRequest(request);
+ 
+            if (formItems != null && formItems.size() > 0) {
+                // iterates over form's fields
+                for (FileItem item : formItems) {
+                    // processes only fields that are not form fields
+                    if (!item.isFormField()) {
+                        String fileName = new File(item.getName()).getName();
+                        String filePath = uploadPath + File.separator + fileName;
+                        LOGGER.log(Level.INFO, filePath);
+                        File storeFile = new File(filePath);
+ 
+                        // saves the file on disk
+                        item.write(storeFile);
+                        request.setAttribute("errorMsg",
+                            "Upload has been done successfully!");
+                    }
+                }
+            }
+        } catch (Exception ex) {
+            request.setAttribute("errorMsg",
+                    "There was an error: " + ex.getMessage());
+        }
+
+
+		requestDispatcher = request
+				.getRequestDispatcher("jsp/upload_article.jsp");
+		requestDispatcher.forward(request, response);
 	}
 }
