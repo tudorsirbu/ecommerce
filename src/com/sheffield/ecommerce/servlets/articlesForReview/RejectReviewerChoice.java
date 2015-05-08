9ee3a1356fcd6fa8f2382f4a30017d5b55993743
benjy2429 <luke.heavens@gmail.com>
2015-05-08 17:38:37 +0100

    removed unused import

--- src/com/sheffield/ecommerce/servlets/articlesForReview/RejectReviewerChoice.java
+++ src/com/sheffield/ecommerce/servlets/articlesForReview/RejectReviewerChoice.java
@@ -1,8 +1,6 @@
 package com.sheffield.ecommerce.servlets.articlesForReview;
 
 import java.io.IOException;
-
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
